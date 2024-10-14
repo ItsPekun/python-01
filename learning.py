4c419ff61cc4a9131ae3be13d951726352748f5c
Pedro Zambrano <espedrozambrano@gmail.com>
2024-10-14 15:25:08 -0400

    first commit now

---
+++ learning.py
@@ -0,0 +1,23 @@
+my_name = "Pedro"
+my_last_name = "Zambrano"
+age = 20
+married = False
+
+print(f"el apellido de {my_name} es {my_last_name}")
+print(type(my_name))
+print(type(age))
+print(type(married))
+
+print(age == 20)
+if age == 20:
+    print(f"{my_name} tiene 20 años")
+
+else:
+    print(f"{my_name} no tiene 20 años")
+
+number_1 = 16
+
+number_2 = 64
+
+# 16 * 4 = 64
+print(number_1 * 4 == number_2)
